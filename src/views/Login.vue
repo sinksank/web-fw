3c371d9a18d476dd4e88d4a622ac4abc0b9bf939
sinksank <1007286821@qq.com>
2025-02-11 19:05:50 +0800

    2

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -19,40 +19,49 @@
   import { ref } from 'vue';
   import { useAuthStore } from '@/stores/auth';
   import { useRouter } from 'vue-router';
-  import apiClient from './ApiClient';
+
   
   const authStore = useAuthStore();
   const router = useRouter();
   
   const username = ref('');
   const password = ref('');
+//   import apiClient from './ApiClient';
+//   const handleLogin = async () => {
+//     if (username.value && password.value) {
+//       try {
+//         const response = await apiClient.post('/login', {
+//           username: username.value,
+//           password: password.value,
+//         });
   
-  const handleLogin = async () => {
-    if (username.value && password.value) {
-      try {
-        const response = await apiClient.post('/login', {
-          username: username.value,
-          password: password.value,
-        });
-  
-        if (response && response.data && response.data.token) {
-          const token = response.data.token;
+//         if (response && response.data && response.data.token) {
+//           const token = response.data.token;
           
-          // 存储 token
-          authStore.login(token, username.value);
+//           // 存储 token
+//           authStore.login(token, username.value);
   
-          alert('登录成功');
-          router.push('/');
-        } else {
-          alert('登录失败，请检查用户名或密码');
-        }
-      } catch (error) {
-        console.error('登录请求出错:', error);
-        alert('登录失败，请稍后重试');
-      }
-    } else {
-      alert('请填写用户名和密码');
-    }
-  };
-  </script>
+//           alert('登录成功');
+//           router.push('/');
+//         } else {
+//           alert('登录失败，请检查用户名或密码');
+//         }
+//       } catch (error) {
+//         console.error('登录请求出错:', error);
+//         alert('登录失败，请稍后重试');
+//       }
+//     } else {
+//       alert('请填写用户名和密码');
+//     }
+//   };
+
+const handleLogin = () => {
+    const token = '123'
+    // 存储 token
+    authStore.login(token, username.value);
+
+    alert('登录成功');
+    router.push('/');
+}
+</script>
   
